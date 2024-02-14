c962e036c68c0089f23259d37b5732fa0f2219e8
Janagan00709 <janagan21.cse@sonatech.ac.in>
2024-02-14 11:38:09 +0530

    0x16 is changed

--- home/challenges/xss.php
+++ home/challenges/xss.php
@@ -10,7 +10,7 @@ if(isset($_GET["firstname"]) && isset($_GET["lastname"]))
         echo "<font color=\"red\">Please enter both fields...</font>";   
     }
     else if($firstname=="<script> alert(1) </script>" and $lastname == "<script> alert(1) </script>"){
-        echo "bYtE_BuStErS{erth.676.y676.yhyu6}";
+        echo "ThReAdS_BuStErS{erth.676.y676.yhyu6}";
     }
     else{
         echo "<font color=\"red\">Please try again to find the flag...</font>";
